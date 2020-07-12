48d6c8b4cb1377e3fb01fcd60e9f39385fd32100
parkhongbeen <pack122@naver.com>
2020-07-12 18:05:30 +0900

    settings, templates, static

--- app/config/urls.py
+++ app/config/urls.py
@@ -1,5 +1,4 @@
 """config URL Configuration
-
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
 Examples:
@@ -13,9 +12,22 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
+from product.views import index, order_success_view
+from . import settings
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index, name='index'),
+    path('payments/complete/', order_success_view, name='order_check')
 ]
+
+urlpatterns += static(
+    # URL 앞부분이 /media/이면
+    prefix=settings.MEDIA_URL,
+    # document_root 위치에서 나머지 path에 해당하는 파일을 리턴
+    document_root=settings.MEDIA_ROOT,
+)
--- app/product/views.py
+++ app/product/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+# Create your views here.
 from django.views.decorators.http import require_POST
 
 from product.models import Product
