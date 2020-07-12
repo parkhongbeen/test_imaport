99289adc5015d71e29a92e71b0f92f536ff4d5ed
parkhongbeen <pack122@naver.com>
2020-07-12 16:58:35 +0900

    product

--- app/product/models.py
+++ app/product/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+
 
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(_('상품이름'), max_length=128)
+    price = models.PositiveIntegerField(_('가격'))
+    image = models.ImageField(_('상품 이미지'), upload_to='images/products')
+    stock = models.PositiveIntegerField(_('재고'))
+
+    class Meta:
+        db_table = 'product'
+        verbose_name = 'product'
+        verbose_name_plural = 'product'
+
+        def __str__(self):
+            return self.name
--- app/product/views.py
+++ app/product/views.py
@@ -1,3 +1,19 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from django.views.decorators.http import require_POST
 
-# Create your views here.
+from product.models import Product
+
+
+def index(request):
+    product = Product.objects.first()
+    return render(request, 'index.html', {'product': product})
+
+
+@login_required
+@require_POST
+def order_success_view(request):
+    imp_uid = request.POST.get('imp_uid')
+    print(request.POST)
+    print(request)
+    return render(request, 'payment_success.html', {'imp_uid', imp_uid})
